66ccd266a9878e6cee04f06fd3a5b70a6a8ad1d1
Rainer Aas <raineraas2@gmail.com>
2023-03-13 16:03:36 +0200

    React-spring-lightbox testing

--- src/views/images/images.component.jsx
+++ src/views/images/images.component.jsx
@@ -1,8 +1,42 @@
+import { useState } from 'react';
+import Lightbox from 'react-spring-lightbox';
+
 function Images() {
+  const images = [
+    {
+      src: 'https://placekitten.com/1024/768?image=1',
+      loading: 'lazy',
+      alt: '',
+    }, {
+      src: 'https://placekitten.com/1024/768?image=2',
+      loading: 'lazy',
+      alt: '',
+    }, {
+      src: 'https://placekitten.com/200/300',
+      loading: 'lazy',
+      alt: '',
+    },
+  ];
+
+  const [isOpen, setOpen] = useState(false);
+  const [currentImageIndex, setCurrentIndex] = useState(0);
+
+  const gotoPrevious = () => currentImageIndex > 0 && setCurrentIndex(currentImageIndex - 1);
+  const gotoNext = () => (
+    currentImageIndex + 1 < images.length && setCurrentIndex(currentImageIndex + 1)
+  );
+
   return (
-    <div>
-      <h1>Images</h1>
-    </div>
+    <>
+      <button type="button" onClick={() => setOpen(true)}>Open</button>
+      <Lightbox
+        isOpen={isOpen}
+        onPrev={gotoPrevious}
+        onNext={gotoNext}
+        images={images}
+        currentIndex={currentImageIndex}
+      />
+    </>
   );
 }
 
