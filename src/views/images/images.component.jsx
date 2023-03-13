b01e5296ec96d2c44b13666c50a3365f47a2a58f
Rainer Aas <raineraas2@gmail.com>
2023-03-13 15:31:45 +0200

    Test yet-another-react-lightbox

--- src/views/images/images.component.jsx
+++ src/views/images/images.component.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable quotes */
+import { useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+// eslint-disable-next-line import/no-unresolved
+import "yet-another-react-lightbox/styles.css";
+
 function Images() {
+  const images = [
+    {
+      src: 'https://placekitten.com/1024/768?image=1',
+      width: '1024',
+      height: '768',
+    }, {
+      src: 'https://placekitten.com/1024/768?image=2',
+      width: '1024',
+      height: '768',
+    }, {
+      src: 'https://placekitten.com/200/300',
+      width: '1024',
+      height: '768',
+    },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      <h1>Images</h1>
-    </div>
+    <>
+      <button type="button" onClick={() => setOpen(true)}>
+        Open
+      </button>
+
+      <Lightbox open={open} close={() => setOpen(false)} slides={images} />
+    </>
   );
 }
 
