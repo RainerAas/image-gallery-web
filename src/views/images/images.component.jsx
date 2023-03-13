7d9428f2a882a4aa25a125e492a74545a8551931
Rainer Aas <raineraas2@gmail.com>
2023-03-13 13:23:32 +0200

    Test react photo gallery

--- src/views/images/images.component.jsx
+++ src/views/images/images.component.jsx
@@ -1,8 +1,32 @@
+import Gallery from 'react-photo-gallery';
+
 function Images() {
+  const photos = [
+    {
+      src: 'https://placekitten.com/1024/768?image=1',
+      width: 4,
+      height: 3,
+    }, {
+      src: 'https://placekitten.com/1024/768?image=2',
+      width: 4,
+      height: 3,
+    }, {
+      src: 'https://placekitten.com/200/300',
+      width: 2,
+      height: 3,
+    }, {
+      src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+      width: 1,
+      height: 1,
+    }, {
+      src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+      width: 3,
+      height: 4,
+    },
+  ];
+
   return (
-    <div>
-      <h1>Images</h1>
-    </div>
+    <Gallery photos={photos} />
   );
 }
 
